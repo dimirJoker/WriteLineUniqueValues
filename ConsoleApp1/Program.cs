243e1cd52c502efef2508dda45b45d6378aec086
dimirJoker <mucnikdmitrij14@gmail.com>
2022-05-27 20:59:08 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WriteLineUniqueValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstList = new List<int>();
+            var secondList = new List<int>();
+            var random = new Random();
+            for (var i = 0; i < 100; i++) // lists filling
+            {
+                firstList.Add(random.Next(100));
+                secondList.Add(random.Next(100));
+            } // lists filling
+            WriteLineUniqueValues(firstList, secondList);
+            Console.WriteLine();
+        }
+        static void WriteLineUniqueValues(List<int> firstList, List<int> secondList)
+        {
+            var tempList = new List<int>();
+            tempList.AddRange(firstList);
+            tempList.AddRange(secondList);
+            var i = 0;
+            while (i < tempList.Count) // primary count
+            {
+                var toKill = false;
+                for (var j = i + 1; j < tempList.Count; j++) // sub count
+                {
+                    if (tempList[j] == tempList[i])
+                    {
+                        tempList.RemoveAt(j); // sub copy killing
+                        toKill = true;
+                    }
+                }
+                if (toKill == true)
+                {
+                    tempList.RemoveAt(i); // primary copy killing
+                }
+                else
+                {
+                    Console.Write($"{tempList[i]}; "); // unique value printing
+                    i++;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
